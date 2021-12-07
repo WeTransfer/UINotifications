bb31e2b1cdc192e1f74ed74f245473eaea35468c
Bas Broek <BasThomas@users.noreply.github.com>
2021-12-07 16:53:00 +0100

    Post accessibility notification in presenter (#61)
    * Post accessibility notification in presenter
    
    Fixes #60
    
    * Update Sources/UINotificationPresenters/UINotificationEaseInOutPresenter.swift
    
    Co-authored-by: Amir Khorsandi <khorsandi@me.com>
    
    * Fix CI
    
    * Add macOS constraint to fix CI
    
    * Bonus: trim whitespace
    
    * Fix file reference
    
    Seems like we refer to a file from UINotifications, but that changed in the CI submodule.
    
    Co-Authored-By: Antoine van der Lee <4329185+AvdLee@users.noreply.github.com>
    
    Co-authored-by: Amir Khorsandi <khorsandi@me.com>
    Co-authored-by: Antoine van der Lee <ajvanderlee@gmail.com>
    Co-authored-by: Antoine van der Lee <4329185+AvdLee@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -1,25 +1,26 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
 import PackageDescription
 
 let package = Package(
     name: "UINotifications",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
+        // dev .macOS(.v10_15)
     ],
     products: [
         // dev .library(name: "DangerDeps", type: .dynamic, targets: ["DangerDependencies"]), // dev
         .library(name: "UINotifications", type: .static, targets: ["UINotifications"])
     ],
     dependencies: [
-        // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"),
-        // dev .package(path: "Submodules/WeTransfer-iOS-CI/Danger-Swift")
+        // dev .package(name: "danger-swift", url: "https://github.com/danger/swift", from: "3.12.3"),
+        // dev .package(name: "WeTransferPRLinter", path: "Submodules/WeTransfer-iOS-CI/WeTransferPRLinter")
     ],
     targets: [
-        // This is just an arbitrary Swift file in the app, that has
-        // no dependencies outside of Foundation, the dependencies section
-        // ensures that the library for Danger gets build also.
-        // dev .target(name: "DangerDependencies", dependencies: ["Danger", "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"]),
+        // dev .target(name: "DangerDependencies", dependencies: [
+        // dev     .product(name: "Danger", package: "danger-swift"),
+        // dev     .product(name: "WeTransferPRLinter", package: "WeTransferPRLinter")
+        // dev ], path: "Submodules/WeTransfer-iOS-CI/DangerFakeSources", sources: ["DangerFakeSource.swift"]),
         .target(name: "UINotifications", path: "Sources")
     ]
 )
--- Sources/UINotificationPresenters/UINotificationEaseInOutPresenter.swift
+++ Sources/UINotificationPresenters/UINotificationEaseInOutPresenter.swift
@@ -12,47 +12,55 @@ import UIKit
 
 /// Presents the Notification with an EaseOut in animation, EaseIn out animation.
 public final class UINotificationEaseOutEaseInPresenter: UINotificationPresenter {
-    
+
     public let presentationContext: UINotificationPresentationContext
     public var dismissTrigger: UINotificationDismissTrigger
     public var state: UINotificationPresenterState = .idle
-    
+
     private let inDuration: TimeInterval = 0.2
     private let outDuration: TimeInterval = 0.2
-    
+
     public required init(presentationContext: UINotificationPresentationContext, dismissTrigger: UINotificationDismissTrigger?) {
         self.presentationContext = presentationContext
         self.dismissTrigger = dismissTrigger ?? UINotificationDurationDismissTrigger(duration: 2.0)
     }
-    
+
     public func present() {
         guard state == .idle else { return }
         state = .presenting
-        
+
         if #available(iOS 11.0, *) {
             presentationContext.notificationView.topConstraint?.constant = 0
         } else {
             presentationContext.notificationView.topConstraint?.constant = presentationContext.notificationView.layoutMargins.top + UIApplication.statusBarHeight
         }
 
+        let content = presentationContext.notification.content
+        let accessibilityAnnouncement = [content.title, content.subtitle]
+            .compactMap { $0 }
+            .filter { !$0.isEmpty }
+            .joined(separator: ", ")
+
+        UIAccessibility.post(notification: .announcement, argument: accessibilityAnnouncement)
+
         UIView.animate(withDuration: inDuration, delay: 0.0, options: UIView.AnimationOptions.curveEaseOut, animations: {
             self.presentationContext.containerWindow.layoutIfNeeded()
         }, completion: { (_) in
             self.state = .presented
-            
+
             self.dismissTrigger.target = self
             if let schedulableDismissTrigger = self.dismissTrigger as? UINotificationSchedulableDismissTrigger {
                 schedulableDismissTrigger.schedule()
             }
         })
     }
-    
+
     public func dismiss() {
         guard state == .presented else { return }
         state = .dismissing
-        
+
         presentationContext.notificationView.topConstraint?.constant = -presentationContext.notification.style.height.value
-        
+
         UIView.animate(withDuration: outDuration, delay: 0, options: UIView.AnimationOptions.curveEaseIn, animations: {
             self.presentationContext.containerWindow.layoutIfNeeded()
         }, completion: { (_) in
