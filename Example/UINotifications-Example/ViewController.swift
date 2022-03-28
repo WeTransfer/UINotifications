e764125aec5bd9a1a04987887ce465ae054d3d94
Kevin Renskers <kevin@loopwerk.io>
2022-03-28 16:40:13 +0200

    More whitespace

--- Example/UINotifications-Example/ViewController.swift
+++ Example/UINotifications-Example/ViewController.swift
@@ -12,7 +12,7 @@ import UINotifications
 enum NotificationStyle: UINotificationStyle {
     case success
     case failure
-    
+
     var titleFont: UIFont {
         switch self {
         case .success:
@@ -21,11 +21,11 @@ enum NotificationStyle: UINotificationStyle {
             return .systemFont(ofSize: 13, weight: .regular)
         }
     }
-    
+
     var subtitleFont: UIFont {
         return .systemFont(ofSize: 13, weight: .regular)
     }
-    
+
     var titleTextColor: UIColor {
         switch self {
         case .success:
@@ -34,11 +34,11 @@ enum NotificationStyle: UINotificationStyle {
             return .white
         }
     }
-    
+
     var subtitleTextColor: UIColor {
         return .darkGray
     }
-    
+
     var backgroundColor: UIColor {
         switch self {
         case .success:
@@ -66,12 +66,12 @@ enum NotificationStyle: UINotificationStyle {
     var maxWidth: CGFloat? {
         return nil
     }
-    
+
     /// When `true`, the notification is swipeable and tappable.
     var interactive: Bool {
         return true
     }
-    
+
     var chevronImage: UIImage? {
         return #imageLiteral(resourceName: "iconToastChevron")
     }
@@ -82,13 +82,13 @@ final class ViewController: UIViewController {
     @IBOutlet private weak var titleTextField: UITextField!
     @IBOutlet private weak var subtitleTextField: UITextField!
     @IBOutlet private weak var showButton: UIButton!
-    
+
     @IBOutlet private weak var automaticallyDismissSwitch: UISwitch!
     @IBOutlet private weak var addActionSwitch: UISwitch!
     @IBOutlet private weak var addButtonSwitch: UISwitch!
     @IBOutlet private weak var showsImageSwitch: UISwitch!
     @IBOutlet private weak var successStyleSwitch: UISwitch!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -102,19 +102,19 @@ final class ViewController: UIViewController {
         var dismissTrigger: UINotificationDismissTrigger = UINotificationDurationDismissTrigger(duration: 2.0)
         let style = successStyleSwitch.isOn ? NotificationStyle.success : NotificationStyle.failure
         let image: UIImage? = showsImageSwitch.isOn ? #imageLiteral(resourceName: "banner") : nil
-        
+
         if automaticallyDismissSwitch.isOn == false {
             dismissTrigger = UINotificationManualDismissTrigger()
         }
-        
+
         if addActionSwitch.isOn {
             action = UINotificationCallbackAction(callback: {
                 print("Tapped the notification!")
             })
         }
-        
+
         let notification = UINotification(content: UINotificationContent(title: title, subtitle: subtitle, image: image), style: style, action: action)
-        
+
         if addButtonSwitch.isOn {
             let button = UIButton(type: .system)
             button.setTitle("Button", for: .normal)
@@ -124,7 +124,7 @@ final class ViewController: UIViewController {
 
         UINotificationCenter.current.show(notification: notification, dismissTrigger: dismissTrigger)
     }
-    
+
     @objc func handleTapNotificationButton() {
         print("Tapped the button!")
     }
